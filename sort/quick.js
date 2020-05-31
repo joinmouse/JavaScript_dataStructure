1791c225029c67851955287db920ff0282b5b60e
joinmouse <908380434@qq.com>
2020-05-31 17:04:13 +0800

    新增快排

---
+++ sort/quick.js
@@ -0,0 +1,40 @@
+/*
+* 快速排序
+* author: joinmouse
+* time: 2020/5/31
+*/
+
+function swap (A, i, j) {
+    [A[i], A[j]] = [A[j], A[i]]
+}
+
+function partitio(A, lo, hi) {
+    // pivot 中心点
+    const pivot = A[hi - 1]
+    let i = lo, j = hi - 1
+    // 小于中心点范围: [lo, i)
+    // 未确定的范围是: [i, j)
+    // 大于中心点范围: [j, hi - 1)
+    while(i !== j) {
+        if(A[i] <= pivot) {
+            i++
+        }else {
+            swap(A, i, --j)
+        }
+    }
+    // 中心点交换
+    swap(A, j, hi-1)
+    return j
+}
+
+function qsort(A, lo = 0, hi = A.length) {
+    if(hi - lo <= 1) return
+    const p = partitio(A, lo, hi)
+    qsort(A, lo, p)
+    qsort(A, p+1, hi)
+}
+
+// 排序test
+const A = [10, 50, 40, 90, 20, 70, 60, 99, 88, 22, 455]
+qsort(A)
+console.log(A)
