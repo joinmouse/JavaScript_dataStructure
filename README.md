1b17a31b4d92519fdc3a08a5ff105a1957c31f14
joinmouse <joinmouse@foxmail.com>
2021-03-15 15:21:43 +0800

    二叉搜索树的基本构造

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
-### 1、排序算法 sort
+## 数据结构篇(核心+重点)
+
+### 1、栈、队列
 
 ### 2、链表 Linked list
 
 ### 3、实现一个hashMap
-- 开放定址法: 
-hashMap/HashMapLine
+- 开放定址法:  hashMap/HashMapLine
 
-- 拉链法: 
-hashMap/HashMapLinked
+- 拉链法: hashMap/HashMapLinked
 
-### 3、动态规划 DP
+### 4、树(tree)
 
-### 4、树 tree
+### 4.1 二叉搜索树: binary search tree
+tree/BinarySearchTree
+
+### 5、图
 - bfs: 广度优先搜索(Breadth-First-Search)
 
         使用的是队列: 先进先出
@@ -20,5 +23,14 @@ hashMap/HashMapLinked
 
         使用的是栈: 先进后出
 
-### 5、常见算法题
-- 数组拍平
+## 算法篇
+
+#### 数组reduce用法总结、数组拍平
+- array/reduce
+- array/flat
+
+#### 排序算法 sort
+
+#### 贪心算法
+
+#### 动态规划
--- tree/binarySearchTree.js
+++ tree/binarySearchTree.js
@@ -4,33 +4,67 @@ class Node {
         this.element = element
         this.left = null
         this.right = null
-    }  
-}
-
-class BST {
-    // 定义一个栈
-    constructor(array) {
-        let [root, ...rest] = array
-        this.queue = [root]
     }
-    // createTree(stack, rest) {
-    //     while(stack.length > 0){
-    //         let top = qu.pop()
-    //         let node = new Node(top)
-    //         if(rest.length >= 2) {
-    //             let left = rest.unshift()
-    //             let right = rest.unshift()
-    //         }
-    //         stack.push(left)
-    //         stack.push(right)
-    //     }
-    // }
 }
 
-let array = [3, 9, 20, null, null, 15, 7]
-/*   3 
-*   / \
-*  9  20
-*     / \
-*    15  7
-*/
+// binary search tree -> 二叉搜索树
+class BST {
+    constructor() {
+        this.root = null
+    }
+    insert(element) {
+        let node = new Node(element)
+        if(this.root === null) {
+            this.root = node
+        }else {
+            let temp = this.root
+            while(temp !== null) {
+                if(element < temp.element) {
+                    if(temp.left === null) {
+                        temp.left = node
+                        return
+                    }else {
+                        temp = temp.left
+                    }
+                }else {
+                    if(temp.right === null) {
+                        temp.right = node
+                        return
+                    }else {
+                        temp = temp.right
+                    }
+                }
+            }
+        }
+    }
+    // 获取二叉树最大高度
+    getHeight(root) {
+        if(root === null) {
+            return 0
+        }
+        let left_height = this.getHeight(root.left)
+        let right_height = this.getHeight(root.right)
+        let max = Math.max(left_height, right_height)
+        // 1是root
+        return max + 1
+    }
+    // 获取二叉树的最大值
+    getMaxElement(root) {
+        if(root === null) {
+            return -1
+        }
+        let leftMaxElement = this.getMaxElement(root.left)
+        let rightMaxElement = this.getMaxElement(root.right)
+        let element = root.element
+        let max = Math.max(leftMaxElement, rightMaxElement, element)
+        return max
+    }
+}
+
+let array = [6, 3, 19, 5, 6, 2, 1, 7]
+let bst = new BST()
+for(let i=0; i<array.length; i++) {
+    bst.insert(array[i])
+}
+console.log("max height is: ", bst.getHeight(bst.root))
+console.log("max element is: ", bst.getMaxElement(bst.root))
